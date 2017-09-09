feb4057fdf721a92b6bd7c2ab476cc7eb4913985
Zeina Sadaldeen <zeina.sadal.deen@gmail.com>
2017-09-09 20:54:51 +0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+// copy index.html to public folder
+require('../index.html');
+require('../../node_modules/foundation-sites/dist/foundation-flex.css');
+require('../css/main.css');
+require('../img/Rock.png');
+require('../img/Paper.png');
+require('../img/Scissors.png');
+require('../img/favicon.ico');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './views/App.jsx';
+import store from './stores/index.js';
+
+const render = () => {
+	ReactDOM.render(
+		<App state={store.getState()} />,
+		document.getElementById('root')
+	);
+};
+
+store.subscribe(render);
+render();
---
+++ src/js/views/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import SetUsername from './SetUsername.jsx';
+import Game from './Game.jsx';
+import store from '../stores/index.js';
+import {setUsername} from '../actions/index.js';
+import classNames from 'classnames';
+
+
+const App = ({state}) => {
+	var subHeader1Class = classNames({
+		'columns': true,
+		'small-8': true,
+		'hide': state.username
+	});
+	var subHeader2Class = classNames({
+		'columns': true,
+		'small-12': true,
+		'hide': !state.username
+	});
+	return (
+		<section>
+			<header>
+				<section id="bar"><h1>Rock Paper Scissors</h1></section>
+				<section className="sub-header row align-center">
+					<h2 className={subHeader1Class}>Ready ?</h2>
+					<h2 className={subHeader2Class}>Let's play!</h2>
+				</section>
+			</header>
+			<main>
+				<section className="row align-center">
+					<section className="columns small-8 medium-6 large-4">
+						<SetUsername onSet={(value) => {
+							store.dispatch(setUsername(value));
+						}} classes={state.username ? 'hide' : ''} />
+					</section>
+				</section>
+			<Game score={state.score} username={state.username} status={state.status} classes={state.username ? '' : 'hide'} />
+			</main>
+			<footer>
+				Created by dev-zeina
+			</footer>
+		</section>
+	)
+};
+
+App.propTypes = {
+	state: React.PropTypes.object.isRequired
+};
+
+export default(
+	App
+);
